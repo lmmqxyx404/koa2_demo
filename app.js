bd70d4752418acd1fc0b1c833a75258cd5a4d89e
司马凇 <1368470804@qq.com>
2020-06-06 23:44:28 +0800

    This is a template demo file

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-const Koa = require('koa')
+const nunjucks = require('nunjucks');
 
-const bodyParser = require('koa-bodyparser')
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
 
-const controller = require('./controller.js')
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
 
-const app = new Koa()
+var s = env.render('hello.html', { name: '小明' });
+console.log(s);
 
-app.use(async (ctx, next) => {
-  console.log(`Process the client request method:${ctx.request.method} ,url:${ctx.request.url}  `);
-  await next()
-})
-
-app.use(bodyParser())
-
-app.use(controller())
-
-app.listen(9999)
-console.log(`app is listening the port :9999`);
+var s = env.render('hello.html', { name: '<script>alert("小明")</script>' });
+console.log(s);
--- controller/hello.js
+++
@@ -1,8 +0,0 @@
-let fn_hello = async (ctx, next) => {
-  let received = ctx.params.name
-  ctx.response.body = `<h1>url is hello parameters is ${received}</h1>`
-}
-
-module.exports = {
-  'GET /hello/:name': fn_hello
-}
